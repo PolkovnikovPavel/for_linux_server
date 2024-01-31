0f9f9759a65e3b4c8da5651febdcd42f403f2c8f
pavel <pavelpolkovnikov334@gmail.com>
2024-01-31 14:33:29 +0700

    4 тест - вернул обычный подход запуска

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> str:
+def index():
     return '<p>Hello Flask!</p>'
 
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # serve(app, host="0.0.0.0", port=80)
